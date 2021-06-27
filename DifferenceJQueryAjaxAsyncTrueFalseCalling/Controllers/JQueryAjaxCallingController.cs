8ea728ece97d2feb7360ea5f4f0d92751963cbef
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-06-27 17:11:55 +0600

    Add project files.

---
+++ DifferenceJQueryAjaxAsyncTrueFalseCalling/Controllers/JQueryAjaxCallingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DifferenceJQueryAjaxAsyncTrueFalseCalling.Controllers
+{
+    public class JQueryAjaxCallingController : Controller
+    {
+        [HttpPost]
+        public IActionResult Response1()
+        {
+            Thread.Sleep(2000);  // Wait 2 Second
+           
+            return Json(
+               new
+               {
+                   message = "1st Response- Success!!!"
+               }
+           );
+        }
+
+        [HttpPost]
+        public IActionResult Response2()
+        {
+            return Json(
+               new
+               {
+                   message = "2nd Response- Success!!!"
+               }
+           );
+        }
+    }
+}
